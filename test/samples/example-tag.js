14b59ef33da9682763d013e6a9a963da7dc147b3
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-18 17:39:56 -0500

    adding full generated html with @example tag

---
+++ test/samples/example-tag.js
@@ -0,0 +1,20 @@
+
+/**
+ * Adds a and b and returns the result.
+ *
+ * @example
+ * // contrived example to test code highlighting
+ * var assert = require('assert')
+ *  , add = require('add');
+ *
+ * var c = add(1, 2);
+ * assert.equal(c, 1 + 2);
+ * 
+ * @name add
+ * @function
+ * @param {Number} a first number
+ * @param {Number} b second number
+ */
+function add(a, b) {
+
+}
